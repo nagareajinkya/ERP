3601af2f5bfa7c332a3a6c78025e7e78dc8b04c5
D1-92797-AJINKYA <nagareajinkya53@gmail.com>
2026-02-04 14:20:43 +0530

    removed the outlet from here and added it in authenticatedlayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AuthenticatedLayout from '../components/layout/AuthenticatedLayout';
 
 import Dashboard from './../pages/Dashboard/Dashboard';
-import Sidebar from '../components/layout/Sidebar';
 import Parties from '../pages/Parties/Parties';
 import NewTransaction from '../pages/Transactions/NewTransactions';
 import Inventory from '../pages/Inventory/Inventory';
@@ -15,34 +15,21 @@ import Transactions from '../pages/Transactions/Transactions';
 import Templates from '../pages/Templates/Templates';
 import Printer from '../pages/Printer/Printer';
 
-// 1. Create a Layout component for the main app
-const MainLayout = () => {
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 flex">
-      <Sidebar />
-      <div className='flex-1 ml-64'>
-        {/* The <Outlet /> renders whatever child route is currently active */}
-        <Outlet /> 
-      </div>
-    </div>
-  );
-};
-
 function App() {
   return (
     <Routes>
       {/* 2. Public Route (No Sidebar) */}
-      <Route 
-        path="/" 
-        element={<Login />} 
+      <Route
+        path="/"
+        element={<Login />}
       />
-      <Route 
-        path="/login" 
-        element={<Login />} 
+      <Route
+        path="/login"
+        element={<Login />}
       />
 
       {/* 3. Protected/Main Routes (With Sidebar) */}
-      <Route element={<MainLayout />}>
+      <Route element={<AuthenticatedLayout />}>
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/parties" element={<Parties />} />
         <Route path="/transactions" element={<Transactions />} />
