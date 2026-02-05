ea2e469007c0097ba8a20a262420125d6efa1cc4
D1-92797-AJINKYA <nagareajinkya53@gmail.com>
2026-02-06 03:01:00 +0530

    removed 1 seeding route:

--- service-smart-ops/routes/smartOpsRoutes.js
+++ service-smart-ops/routes/smartOpsRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../auth');
-const { calculate, seedOffers } = require('../controllers/smartOpsController');
+const { calculate } = require('../controllers/smartOpsController');
 
 router.post('/calculate', auth, calculate);
-router.post('/seed', auth, seedOffers);
 
 module.exports = router;
