2694653305d39118ef593f06d0fbb5588ed4d3cf
D1-92797-AJINKYA <nagareajinkya53@gmail.com>
2026-02-06 15:39:43 +0530

    added bean for creating restTemplate singleton instance

--- service-trading/src/main/java/com/sbms/trading_service/ServiceTradingApplication.java
+++ service-trading/src/main/java/com/sbms/trading_service/ServiceTradingApplication.java
@@ -6,6 +6,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ServiceTradingApplication {
@@ -22,5 +23,10 @@ public class ServiceTradingApplication {
 		.setPropertyCondition(Conditions.isNotNull());
 		
 		return modelMapper;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
